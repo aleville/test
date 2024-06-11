f7ce0f7f196c5b8f0bc00e79b8cde277d0e44d15
aleville <51177908+aleville@users.noreply.github.com>
2024-06-11 10:52:42 -0400

    Update sml2ex1.c

--- sml2ex1.c
+++ sml2ex1.c
@@ -7,8 +7,8 @@
 int main(int argc, char** argv) {
     // Ensure that the user supplied exactly one command line argument
     if (argc != 2) { 
-    fprintf(stderr, "Please provide the address of a file as an input.\n");
-    return -1;
+        fprintf(stderr, "Please provide the address of a file as an input.\n");
+        return -1;
     }
     char cmd[BUFSIZE] = "wc -c < ";
     strcat(cmd, argv[1]);
